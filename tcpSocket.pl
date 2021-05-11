5c391a15a42e122c1316fae314098b6de82c5824
kamleong <kamleong@users.noreply.github.com>
2021-05-11 22:07:39 +0800

    Update tcpSocket.pl

--- tcpSocket.pl
+++ tcpSocket.pl
@@ -1,22 +1,33 @@
-#!/usr/bin/perl
-
 use IO::Socket::INET;
 
 sub sendData {
   my ($targetHost, $targetPort, $reqString) = @_;
-  my $respString = "";
-  #return $respString;
+  my $buf="", $respString="";
   my $socket = new IO::Socket::INET (
-    PeerHost => $targetHost, PeerPort => $targetPort, Proto => 'tcp',
+    PeerHost => $targetHost, PeerPort => $targetPort,
+    Proto => 'tcp', Type=>IO::Socket::SOCK_STREAM
   ); return "cannot connect to the server $!n" unless $socket;
 
+  $socket->autoflush(1);  # http://www.rocketaware.com/perl/perlipc/TCP_Clients_with_IO_Socket.htm
+  print $socket $reqString;
+  while (defined($buf=<$socket>)) {
+    $respString .= $buf;
+  }
+
   # data to send to a server
-  my $size = $socket->send($reqString);  #print "sent data of length $sizen";
-  shutdown($socket, 1);  # notify server that request has been sent
-  # receive a response of up to 4096 characters from server
-  $socket->recv($respString, 4096);
+  # my $size = $socket->send($reqString);  #print "sent data of length $sizen";
+  # shutdown($socket, 1);  # notify server that request has been sent
+  # $socket->recv($respString, 4096);  # receive a response of up to 4096 characters from seerver
+
   $socket->close();
   return $respString;
 }
 
-print sendData("www.google.com", "80", "GET /");
+#print sendData("www.google.com", "80", "GET / HTTP/1.0 \r\n\r\n");
+
+print sendData(
+  "info.cern.ch", "80", "GET / HTTP/1.0\r\n"
+  . "Host: info.cern.ch\r\n"
+  . "\r\n"
+);
+
