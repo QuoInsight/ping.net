bf53efa673dfce3637ea4dac75051b4d5bd754e2
kamleong <kamleong@users.noreply.github.com>
2022-07-25 19:32:43 +0800

    Update DNSProxyServer.py

--- DNSProxyServer.py
+++ DNSProxyServer.py
@@ -8,7 +8,7 @@
 #   dig -p 6760 @127.0.0.1 google.com
 #   dig -p 6760 @127.0.0.1 +tcp google.com
 #   nslookup - 127.0.0.1
-#    set port=6760
+#    set port=6760 ## not working on Windows10 https://www.reddit.com/r/sysadmin/comments/eavx11/nslookup_on_windows_is_broken_and_ignores_command/
 #    google.com
 # 
 # If an UDP DNS request is coming, the server will start a new thread
@@ -38,14 +38,182 @@
 
 import socket
 import sys
-import struct
 import _thread ## The thread module has been "deprecated" for quite a long time.
                ## Users are encouraged to use the threading module instead.
                ## Hence, in Python 3, the module "thread" is not available anymore.
                ## However, it has been renamed to "_thread" for backwards compatibilities in Python3.
 import codecs
 
-# send a TCP DNS query to the upstream DNS server
+def printDnsMsg(msgByteArr):
+  msgHdr = codecs.encode(msgByteArr[:12], "hex").decode()
+  print(msgHdr)
+
+  ## ID:2bytes; Flags+OpCode+RespCode:2bytes; RecordsCount:4*2bytes
+  #rcode = int(msgHdr[7], 16)
+  #print(bin(query1[2]))
+
+  byte3 = format(msgByteArr[2],"08b");  #print(byte3)
+  qryRspFlg = int(byte3[0])  ## 0:qry|1:rsp
+  opCode = int(byte3[1:5], 2) ## 0:qry|1:invQry|2:status|4:notify|5:update
+
+  byte4 = format(msgByteArr[3],"08b");  #print(byte4)
+  rspCode = int(byte4[4:], 2) ## 0:noErr|1:formatErr|2:serverErr|3:nameErr|5:refused
+  qCount = int.from_bytes(msgByteArr[4:6], 'big') ## struct.unpack('>H', "AB00")
+  aCount = int.from_bytes(msgByteArr[6:8], 'big')
+
+  print(
+    "[Header] qryRspFlg:"+str(qryRspFlg)
+     + " opCode:"+str(opCode) + " rspCode:"+str(rspCode)
+      + " qCount:"+str(qCount) + " aCount:"+str(aCount)
+  )
+
+  ## https://cabulous.medium.com/dns-message-how-to-read-query-and-response-message-cfebcb4fe817
+  byteIdx = 12  ## byte#13 is the size of first string in the first DNS query/question
+  qName1 = ""
+  while True:
+    sz1 = msgByteArr[byteIdx];  byteIdx = byteIdx +1;
+    if sz1==0 : break
+    byteIdx2 = byteIdx + sz1;
+    qName1 = qName1 + msgByteArr[byteIdx:byteIdx2].decode() + "."
+    byteIdx = byteIdx2
+  #
+  qType = int.from_bytes(msgByteArr[byteIdx:byteIdx+2], 'big') ## 1:A(ipv4)|28:AAAA(ipv6)| https://en.wikipedia.org/wiki/List_of_DNS_record_types#Resource_records
+  qClass = int.from_bytes(msgByteArr[byteIdx+2:byteIdx+4], 'big') ## normally the value 1 for Internet ('IN')
+  byteIdx = byteIdx+4 ## end of q1
+
+  print("qName1:"+str(qName1))
+  print("qType:"+str(qType))
+  print("qClass:"+str(qClass))
+
+  if (qryRspFlg==1 and qCount==1 and aCount > 0):
+    aNameOffset = format(msgByteArr[byteIdx],"08b") ## 4.1.4. Message compression https://datatracker.ietf.org/doc/html/rfc1035
+    # In order to reduce the size of messages, the domain system utilizes a
+    # compression scheme which eliminates the repetition of domain names in a
+    # message.  In this scheme, an entire domain name or a list of labels at
+    # the end of a domain name is replaced with a pointer to a prior occurance
+    # of the same name.
+    if (aNameOffset[:2]=="11" and msgByteArr[byteIdx+2]==0) :
+      aNameOffset = aNameOffset[2:] + format(msgByteArr[byteIdx+1],"08b")
+      aNameOffset = int(aNameOffset, 2)
+      print("aNameOffset:"+str(aNameOffset))
+      # we will simply use qName1 above and advance 2 bytes below to the next data
+      aName1 = qName1
+      byteIdx = byteIdx + 2
+    else :
+      aNameOffset = 0
+      aName1 = ""
+      while True:
+        sz1 = msgByteArr[byteIdx];  byteIdx = byteIdx +1;
+        if sz1==0 : break
+        byteIdx2 = byteIdx + sz1;
+        aName1 = aName1 + msgByteArr[byteIdx:byteIdx2].decode() + "."
+        byteIdx = byteIdx2
+      #
+    #
+
+    aType = int.from_bytes(msgByteArr[byteIdx:byteIdx+2], 'big') ## 1:A(ipv4)|28:AAAA(ipv6)| https://en.wikipedia.org/wiki/List_of_DNS_record_types#Resource_records
+    aClass = int.from_bytes(msgByteArr[byteIdx+2:byteIdx+4], 'big') ## normally the value 1 for Internet ('IN')
+    byteIdx = byteIdx+4 ##
+
+    print(msgByteArr[byteIdx:byteIdx+4])
+    aTtl = int.from_bytes(msgByteArr[byteIdx:byteIdx+4], 'big') 
+    byteIdx = byteIdx+4 ##
+    print("aTtl:"+str(aTtl))
+
+    aSize = int.from_bytes(msgByteArr[byteIdx:byteIdx+2], 'big') 
+    byteIdx = byteIdx+2 ##
+    print("aSize:"+str(aSize))
+    if (aSize==4) :
+      aAddr = ( str(msgByteArr[byteIdx]) + "." + str(msgByteArr[byteIdx+1])
+        + "." + str(msgByteArr[byteIdx+2])  + "." + str(msgByteArr[byteIdx+3])
+      )
+    elif (aSize==16) :
+      ## eight groups of four hexadecimal digits
+      aAddr = (
+        codecs.encode(msgByteArr[byteIdx:byteIdx+2],"hex").decode()
+        + ":" + codecs.encode(msgByteArr[byteIdx+2:byteIdx+4],"hex").decode()
+        + ":" + codecs.encode(msgByteArr[byteIdx+4:byteIdx+6],"hex").decode()
+        + ":" + codecs.encode(msgByteArr[byteIdx+6:byteIdx+8],"hex").decode()
+        + ":" + codecs.encode(msgByteArr[byteIdx+8:byteIdx+10],"hex").decode()
+        + ":" + codecs.encode(msgByteArr[byteIdx+10:byteIdx+12],"hex").decode()
+        + ":" + codecs.encode(msgByteArr[byteIdx+12:byteIdx+14],"hex").decode()
+        + ":" + codecs.encode(msgByteArr[byteIdx+14:byteIdx+16],"hex").decode()
+      )
+    else :
+      aAddr = "?"
+    #
+    print("aAddr:"+str(aAddr))
+
+  #
+  #struct.unpack
+#
+
+def disableIpv6(msgByteArr) :
+  byte3 = format(msgByteArr[2],"08b");  #print(byte3)
+  qryRspFlg = int(byte3[0])  ## 0:qry|1:rsp
+  opCode = int(byte3[1:5], 2) ## 0:qry|1:invQry|2:status|4:notify|5:update
+  byte4 = format(msgByteArr[3],"08b");  #print(byte4)
+  rspCode = int(byte4[4:], 2) ## 0:noErr|1:formatErr|2:serverErr|3:nameErr|5:refused
+  qCount = int.from_bytes(msgByteArr[4:6], 'big') ## struct.unpack('>H', "AB00")
+  aCount = int.from_bytes(msgByteArr[6:8], 'big')
+  if (qryRspFlg==1 and qCount==1 and aCount > 0):
+    ## https://cabulous.medium.com/dns-message-how-to-read-query-and-response-message-cfebcb4fe817
+    byteIdx = 12  ## byte#13 is the size of first string in the first DNS query/question
+    qName1 = ""
+    while True:
+      sz1 = msgByteArr[byteIdx];  byteIdx = byteIdx +1;
+      if sz1==0 : break
+      byteIdx2 = byteIdx + sz1;
+      qName1 = qName1 + msgByteArr[byteIdx:byteIdx2].decode() + "."
+      byteIdx = byteIdx2
+    #
+    qType = int.from_bytes(msgByteArr[byteIdx:byteIdx+2], 'big') ## 1:A(ipv4)|28:AAAA(ipv6)| https://en.wikipedia.org/wiki/List_of_DNS_record_types#Resource_records
+    qClass = int.from_bytes(msgByteArr[byteIdx+2:byteIdx+4], 'big') ## normally the value 1 for Internet ('IN')
+    byteIdx = byteIdx+4 ## end of q1
+    if (qType==28) :
+      aNameOffset = format(msgByteArr[byteIdx],"08b") ## 4.1.4. Message compression https://datatracker.ietf.org/doc/html/rfc1035
+      # In order to reduce the size of messages, the domain system utilizes a
+      # compression scheme which eliminates the repetition of domain names in a
+      # message.  In this scheme, an entire domain name or a list of labels at
+      # the end of a domain name is replaced with a pointer to a prior occurance
+      # of the same name.
+      if (aNameOffset[:2]=="11" and msgByteArr[byteIdx+2]==0) :
+        aNameOffset = aNameOffset[2:] + format(msgByteArr[byteIdx+1],"08b")
+        aNameOffset = int(aNameOffset, 2)
+        # we will simply use qName1 above and advance 2 bytes below to the next data
+        aName1 = qName1
+        byteIdx = byteIdx + 2
+      else :
+        aNameOffset = 0
+        aName1 = ""
+        while True:
+          sz1 = msgByteArr[byteIdx];  byteIdx = byteIdx +1;
+          if sz1==0 : break
+          byteIdx2 = byteIdx + sz1;
+          aName1 = aName1 + msgByteArr[byteIdx:byteIdx2].decode() + "."
+          byteIdx = byteIdx2
+        #
+      #
+      aType = int.from_bytes(msgByteArr[byteIdx:byteIdx+2], 'big') ## 1:A(ipv4)|28:AAAA(ipv6)| https://en.wikipedia.org/wiki/List_of_DNS_record_types#Resource_records
+      aClass = int.from_bytes(msgByteArr[byteIdx+2:byteIdx+4], 'big') ## normally the value 1 for Internet ('IN')
+      byteIdx = byteIdx+4 ##
+      if (aType==28) :
+        aTtl = int.from_bytes(msgByteArr[byteIdx:byteIdx+4], 'big') 
+        byteIdx = byteIdx+4 ##
+        aSize = int.from_bytes(msgByteArr[byteIdx:byteIdx+2], 'big') 
+        byteIdx = byteIdx+2 ##
+        if (aSize==16) :
+          print("disableIpv6 ...")
+          # for i in range(aSize): msgByteArr[byteIdx+i] = 0 ## Error: 'bytes' object does not support item assignment
+          msgByteArr = b''.join([msgByteArr[:byteIdx], bytearray(aSize), msgByteArr[byteIdx+aSize:]])
+        #
+      #
+    #
+  #
+  return msgByteArr
+#
+
+# send DNS query to the upstream DNS server
 def sendUpstrmQuery(server, port, sockTyp, qryMsg):
   sock = socket.socket(socket.AF_INET, sockTyp)
   print("sendUpstrmQuery "+server+":"+str(port)+" ... "+str(sock.type))
@@ -92,6 +260,7 @@ def handler(data, addr, c, sock, upstrmServer, upstrmPort, upstrmSockTyp):
       udpAnswer = upstrmAnswer
       tcpAnswer = addMsgSzPrefix(upstrmAnswer)
     #
+    udpAnswer = disableIpv6(udpAnswer)
     try :
       msgHeader = codecs.encode(udpAnswer[:12], "hex").decode() # total:4+8bytes
       print("msgHeader: ", msgHeader) ## [ID:0123][Flag&Codes:4567][RecordCounts:8901,2345,6789,0123]
@@ -120,8 +289,8 @@ def handler(data, addr, c, sock, upstrmServer, upstrmPort, upstrmSockTyp):
 if __name__ == '__main__':
   upstrmServer = "8.8.8.8" ## "192.168.43.1" ## "8.8.8.8" ## sys.argv[1] ## !! 192.168.192.1 ==> tcpNotSupported ## 
   upstrmPort = 53
-  upstrmSockTyp = socket.SOCK_STREAM ## udp: socket.SOCK_DGRAM | tcp:socket.SOCK_STREAM
-  localhost = '' ## '127.0.0.1'
+  upstrmSockTyp = socket.SOCK_DGRAM ## udp: socket.SOCK_DGRAM | tcp:socket.SOCK_STREAM
+  localhost = '127.0.0.1' ## '' | '127.0.0.1'
   localport = 6760 ## 53 ## int(sys.argv[2])
   localSockTyp = socket.SOCK_DGRAM ## udp: socket.SOCK_DGRAM | tcp:socket.SOCK_STREAM
   if len(sys.argv) > 2 :
