ffa64aa11b567963f5044503a7da140a503e1c46
kamleong <kamleong@users.noreply.github.com>
2022-08-17 14:53:12 +0800

    Update udpProxy.lua

--- socket/udpProxy.lua
+++ socket/udpProxy.lua
@@ -17,13 +17,51 @@ end
 
 ------------------------------------------------------------------------
 
+local function getDeviceLanIpAddr()
+  local function _exeCmd(cmdln)
+    local file = assert(io.popen(cmdln, 'r'))
+    local output = file:read('*all')
+    file:close()
+    output = string.gsub(output, "^%s*(.-)%s*$", "%1") --trim
+    return output
+  end
+  local cmdln = "ifconfig br-lan | grep -F 'inet addr:' | sed -r 's/^\s*inet addr:([^ ]+).*/\1/'"
+  return _exeCmd(cmdln)
+end
+
+local function getMachineIpAddr()
+  addr,details = socket.dns.toip(socket.dns.gethostname())
+  return addr
+end
+
+local function getInterfaceIpAddr(sckt)
+  local pcallOK,addr,port = pcall( function() return sckt:getpeername() end )
+  if not pcallOK then sckt:setpeername("12.34.56.78",12345) end
+  local addr,port,fmly = sckt:getsockname()
+  if pcallOK then
+    sckt:setpeername(addr,port)
+  else
+    sckt:setpeername('*')
+  end
+  return addr
+end
+
+------------------------------------------------------------------------
+
 local socket = require("socket")
 
 local upstrmSckt = socket.udp()
 upstrmSckt:settimeout(5)
 
 local svcSckt = socket.udp()
+if svcAddr=='*' then svcAddr=getDeviceLanIpAddr() end
+-- somehow svcAddr='*'|INADDR_ANY is not reliable and --
+-- causing svcSckt:sendto() not received correctly by --
+-- some downstream clients (e.g. nslookup/dig/py) !!! --
+-- no issue with INADDR_ANY for loopback connections, --
+-- or with luasocket udp as remote client !??         --
 svcSckt:settimeout(5)
+svcSckt:setoption('reuseaddr',true)
 svcSckt:setsockname(svcAddr, svcPort)
 
 print "Beginning server loop ..."
